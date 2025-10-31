4b23200a7ae1b024368722b8bfb2b9dd6121aebb
GJoseI <joseignaciogauna2@gmail.com>
2025-10-31 18:04:14 -0300

    Preparacion crear alarma

--- app/src/main/java/com/example/apprecordatorio/activities/CrearAlarmaActivity.java
+++ app/src/main/java/com/example/apprecordatorio/activities/CrearAlarmaActivity.java
@@ -1,6 +1,10 @@
 package com.example.apprecordatorio.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
 
@@ -11,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.apprecordatorio.R;
+import com.example.apprecordatorio.fragments.AlarmasFragment;
 
 public class CrearAlarmaActivity extends AppCompatActivity {
     private TimePicker tpHora;
@@ -21,6 +26,10 @@ public class CrearAlarmaActivity extends AppCompatActivity {
     private ToggleButton btJue;
     private ToggleButton btVie;
     private ToggleButton btSa;
+    private Spinner spAlarmaTono;
+    private EditText etDesc;
+    private Button btCancelar;
+    private Button btGuardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +41,31 @@ public class CrearAlarmaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        innitVars();
+
+        btGuardar.setOnClickListener(view -> guardarAlarma());
+        btCancelar.setOnClickListener(view -> volverAtras());
+    }
+
+    public void volverAtras(){
+        Intent i = new Intent(this, AlarmasFragment.class);
+        startActivity(i);
+    }
+
+    public void guardarAlarma(){
+
+    }
+
+    public void innitVars(){
+        tpHora.findViewById(R.id.tpHora);
+        btDom.findViewById(R.id.btDom);
+        btLu.findViewById(R.id.btLu);
+        btMar.findViewById(R.id.btMar);
+        btMie.findViewById(R.id.btMie);
+        btJue.findViewById(R.id.btJue);
+        btVie.findViewById(R.id.btVie);
+        btSa.findViewById(R.id.btSa);
+        spAlarmaTono.findViewById(R.id.spAlarmaTono);
+        etDesc.findViewById(R.id.etDesc);
     }
 }
