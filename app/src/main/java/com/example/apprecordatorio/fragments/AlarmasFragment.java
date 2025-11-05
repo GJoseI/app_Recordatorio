ba25ec3c38e9e6604c6467f4d55146f43041e04e
santibritos <116766975+santibritos@users.noreply.github.com>
2025-11-05 19:37:18 -0300

    color card alarma

--- app/src/main/java/com/example/apprecordatorio/fragments/AlarmasFragment.java
+++ app/src/main/java/com/example/apprecordatorio/fragments/AlarmasFragment.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -50,7 +52,17 @@ public class AlarmasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarmas, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_alarmas, container, false);
+
+
+        ///  cargando ejemplo de card
+        LinearLayout containerRec = view.findViewById(R.id.containerRecordatoriosAlarmas);
+
+       View card =  inflater.inflate(R.layout.item_recordatorio,containerRec,false);
+        containerRec.addView(card);
+
+        return view;
     }
 
     @Override
@@ -97,6 +109,12 @@ public class AlarmasFragment extends Fragment {
            //codigo editar rec
         });
          */
+
+        ///  ACA CARGO UNA CARD PARA EL EJEMPLO DE LAS ALARMAS
+
+
+
+
     }
 
     //  funcion aux para actualizar la lista
