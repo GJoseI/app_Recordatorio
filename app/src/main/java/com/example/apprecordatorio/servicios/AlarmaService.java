ffdae5e22f71b13dd176628ffda875c4baa233a5
GJoseI <joseignaciogauna2@gmail.com>
2025-11-13 19:49:00 -0300

    Heads up alarma

--- app/src/main/java/com/example/apprecordatorio/servicios/AlarmaService.java
+++ app/src/main/java/com/example/apprecordatorio/servicios/AlarmaService.java
@@ -1,13 +1,18 @@
 package com.example.apprecordatorio.servicios;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -32,7 +37,23 @@ public class AlarmaService extends Service {
         String descripcion = intent.getStringExtra("descripcion");
         String imagen = intent.getStringExtra("imagen");
 
-        // Intent para abrir la Activity al tocar la notificación o en pantalla bloqueada
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//        AudioAttributes attributes = new AudioAttributes.Builder()
+//                .setUsage(AudioAttributes.USAGE_ALARM)
+//                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+//                .build();
+            NotificationChannel channel = new NotificationChannel(
+                    "alarma_channel",
+                    "Alarmas",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel.setDescription("Notificaciones de alarma");
+            channel.setSound(null, null);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+                // Intent para abrir la Activity al tocar la notificación o en pantalla bloqueada
         Intent i = new Intent(this, AlarmaActivity.class);
         i.putExtra("titulo", titulo);
         i.putExtra("descripcion", descripcion);
@@ -50,9 +71,9 @@ public class AlarmaService extends Service {
                 .setContentText(descripcion)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setCategory(NotificationCompat.CATEGORY_ALARM)
+                .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
                 .setFullScreenIntent(fullScreenIntent, true)
-                .setOngoing(false)
-                        .setSilent(true);
+                .setOngoing(false).setAutoCancel(true);
 
         NotificationManagerCompat.from(this).notify(1, builder.build());
         startForeground(1, builder.build());
